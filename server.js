1d37299fbc742c75339e6c4092ddbff462e09aaf
Ariel Alvarez <ariel.s.alvarez@gmail.com>
2023-11-24 10:38:55 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const { Pool } = require("pg");
+
+const app = express();
+
+const pool = new Pool({
+    user: 'pacs',
+    host: '200.127.57.176',
+    database: 'pacsdb',
+    password: 'pacs',
+    port: 5432,
+});
+
+function emptyOrRows(rows) {
+    if (!rows) {
+      return [];
+    }
+    return rows;
+  }
+
+async function getStudyUID(an = null) {
+    const rows = await pool.query(
+      'SELECT patient.pk as patientID, study.study_iuid as studyUID FROM study inner join patient on patient.pk = study.patient_fk where study.accession_no = $1', 
+      [an]
+    );
+    const data = emptyOrRows(rows);
+  
+    return {
+      data
+    }
+  }
+  
+app.get('/api/study/urlByAccessionNumber', async function(req, res, next) {
+    // El parametro an (accession number) es requerido
+    if (!req.query.an) {
+        return res.status(422).json({ error: 'Param an (Accession Number) is required' });
+    }
+    try {
+        const r = (await getStudyUID(req.query.an));
+        if (r.data.rowCount) {
+            data = {
+                success: true,
+                url: 'http://imagenes.hemodinamiadelsur.com:3000/viewer/' + r.data.rows[0].studyuid,
+                message: ''
+            }
+        } else {
+            data = {
+                success: false,
+                url: '',
+                message: 'Imágenes no encontradas'
+            }
+        }
+        res.json(data);
+    } catch (err) {
+        console.error(`Error while getting quotes `, err.message);
+      next(err);
+    }
+  });
+
+// Start the server
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
+
